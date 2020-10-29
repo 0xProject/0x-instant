3a37a16e8864b73219afe8d4ea6b3b265af270bf
Jacob Evans <jacob@dekz.net>
2020-10-29 13:19:35 +1000

    initial

---
+++ src/globals.d.ts
@@ -0,0 +1,13 @@
+declare module '*.svg' {
+    const content: any;
+    /* tslint:disable */
+    export default content;
+    /* tslint:enable */
+}
+declare module '*.json' {
+    const json: any;
+    /* tslint:disable */
+    export default json;
+    /* tslint:enable */
+}
+declare module 'fortmatic';
